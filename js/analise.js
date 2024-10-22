eede7a1eccac2e1aec89294b33ec23b03642d42b
yohangoffermann <yohangoffermann@gmail.com>
2024-10-22 12:25:51 -0300

    Update analise.js

--- js/analise.js
+++ js/analise.js
@@ -1,5 +1,4 @@
 function atualizarAnalise(fluxo, parametros) {
-    console.log("atualizarAnalise chamada com:", fluxo, parametros);
     const lucro_total = fluxo.reduce((sum, item) => sum + item['Saldo Mensal'], 0);
     const margem = (lucro_total / parametros.vgv) * 100;
     const exposicao_maxima = -Math.min(...fluxo.map(item => item['Saldo Acumulado']));
